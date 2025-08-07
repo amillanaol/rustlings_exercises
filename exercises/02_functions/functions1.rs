8d58791c4311c07417f8b8773812719421c95a8e
amillanaol <alexis.millanao@hotmail.com>
2025-08-07 17:00:25 -0400

    fix(02_functions): functions1.rs resolved

--- exercises/02_functions/functions1.rs
+++ exercises/02_functions/functions1.rs
@@ -1,5 +1,9 @@
-// TODO: Add some function with the name `call_me` without arguments or a return value.
+// FIXED Add some function with the name `call_me` without arguments or a return value.
 
 fn main() {
     call_me(); // Don't change this line
+}
+
+fn call_me() {
+    println("Hola desde call_me");
 }
