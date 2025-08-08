d6213665524a053f13530c7ae08fbd17c16f39cc
amillanaol <alexis.millanao@hotmail.com>
2025-08-08 17:42:05 -0400

    fix(02_functions): functions4.rs resolved

--- exercises/02_functions/functions4.rs
+++ exercises/02_functions/functions4.rs
@@ -1,4 +1,3 @@
-
 // This store is having a sale where if the price is an even number, you get 10
 
 // Don't worry about the function bodies themselves, we are only interested in
@@ -8,8 +7,8 @@ fn is_even(num: i64) -> bool {
     num % 2 == 0
 }
 
-// TODO: Fix the function signature.
-fn sale_price(price: i64) -> {
+// FIXED: Fix the function signature.
+fn sale_price(price: i64) -> i64 {
     if is_even(price) {
         price - 10
     } else {
